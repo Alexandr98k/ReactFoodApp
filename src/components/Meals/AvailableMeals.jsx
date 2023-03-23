9afc7f1ac389e6029c46fef79d92ae623cb3175c
Alexandr98k <oleksandr.karpenko98@gmail.com>
2023-03-23 14:20:05 +0200

    Added loading state and error handling, http request to database

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,37 +1,70 @@
+import { useEffect, useState } from 'react';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 
 import styles from './AvalilableMeals.module.css';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = function () {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  //відразу передаємо тру, тому що це завантаження відбувається тільки на початку запуску програми.
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(null);
+  //В юз-ефекті функція не може бути асинхронною, тому потрібно передавати створити та викликати ще одну функцію, якщо вона повертає проміс. Тому що наша функція запиту до сервера повертає проміс.
+  useEffect(() => {
+    const fetchMeals = async function () {
+      const response = await fetch(
+        'https://react-http-post-f7e5a-default-rtdb.europe-west1.firebasedatabase.app/meals.json',
+      );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong :(');
+      }
+
+      const data = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+          key: key,
+        });
+      }
+
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+    //використовуємо кетч метод, тому що фетчМілз функція повертає проміз, який також потрібно зачекати.
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setIsError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={styles['meals-loading']}>
+        <Card>
+          <p>Loading...</p>
+        </Card>
+      </section>
+    );
+  }
+
+  if (isError) {
+    return (
+      <section className={styles['meals-error']}>
+        <Card>
+          <p>{isError}</p>
+        </Card>
+      </section>
+    );
+  }
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
